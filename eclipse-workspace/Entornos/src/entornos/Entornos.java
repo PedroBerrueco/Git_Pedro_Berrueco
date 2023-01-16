17ed349b65b2a4f6c5399cd76a0aba4fcd70f526
pedroberrueco <pedroberrueco@hotmail.com>
2023-01-16 16:48:47 +0100

    commit en develop

--- eclipse-workspace/Entornos/src/entornos/Entornos.java
+++ eclipse-workspace/Entornos/src/entornos/Entornos.java
@@ -5,6 +5,7 @@ public class Entornos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola Mundo");
+		System.out.println("Esto es u cambio en la rama develop");
 	}
 
 }
